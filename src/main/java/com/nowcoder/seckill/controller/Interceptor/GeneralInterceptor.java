d12d70d2106610bb3219d5fcfa7488deaae60ffa
Hang Zou <samzou1997@gmail.com>
2021-07-03 18:42:04 +0800

    查看请求者IP

--- src/main/java/com/nowcoder/seckill/controller/Interceptor/GeneralInterceptor.java
+++ src/main/java/com/nowcoder/seckill/controller/Interceptor/GeneralInterceptor.java
@@ -1,8 +1,6 @@
 package com.nowcoder.seckill.controller.Interceptor;
 
-import com.alibaba.fastjson.JSONObject;
 import com.nowcoder.seckill.common.ErrorCode;
-import com.nowcoder.seckill.common.ResponseModel;
 import com.nowcoder.seckill.entity.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -10,27 +8,42 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Component
 public class GeneralInterceptor implements HandlerInterceptor, ErrorCode {
+
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("loginUser");
+        String ip = getIpAddr(request);
         if (user == null) {
-            System.out.println("[User: NOT_LOGIN" + "] TIME: " + System.currentTimeMillis() + " || " + "REQUEST PATH: "
+            System.out.println("[IP: " + ip + " || User: NOT_LOGIN" + "] TIME: " + System.currentTimeMillis() + " || " + "REQUEST PATH: "
                     + request.getServletPath());
         }
         else {
-            System.out.println("[User: " + user.getId() + "] TIME: " + System.currentTimeMillis() + " || " + "REQUEST PATH: "
+            System.out.println("[IP: " + ip + " || User: " + user.getId() + "] TIME: " + System.currentTimeMillis() + " || " + "REQUEST PATH: "
                     + request.getServletPath());
         }
 
         return true;
     }
+
+
 }
